797b9cc9200d9877dcf469f2101fdbcb82ff2839
nurzhana <nurzhana@MacBook-Air-admin.local>
2024-01-31 10:49:13 +0300

    router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HashRouter, Routes, Route} from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx';
 import Liked from './pages/liked/Liked.jsx';
 import './index.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path:'/liked',
+    element: <Liked/>
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
-    <Routes>
-      <Route path='/' element={<App/>}/>
-      <Route path='/liked' element={<Liked/>}/>
-    </Routes>
-  </HashRouter>
+  <RouterProvider router={router}/>
 );
